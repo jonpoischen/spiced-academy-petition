f952e32af3be15142ed24ced02ecfd48a714da93
Jon <jonpoischen@gmail.com>
2018-10-08 17:19:30 +0200

    Did part 3 of the petition

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const spicedPg = require('spiced-pg');
+const bcrypt = require('bcryptjs');
 const {dbUser, dbPassword} = require('./secrets.json')
 const db = spicedPg(`postgres:${dbUser}:${dbPassword}@localhost:5432/sage`);
 
@@ -16,6 +17,26 @@ const showSig = function (id) {
     return db.query(`SELECT signature FROM signatures WHERE id = $1`, [id])
         .then(function(result) {
             return result.rows[0].signature;})
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+const getLoginId = function (email) {
+    return db.query(`SELECT id FROM users WHERE email = $1`, [email])
+        .then(function(result) {
+            return result.rows[0].id;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+
+const getUserName = function (id) {
+    return db.query(`SELECT first, last FROM users WHERE id = $1`, [id])
+        .then(function(results) {
+            return results.rows[0];
+        })
         .catch(err => {
             console.log(err);
         })
@@ -41,7 +62,55 @@ const showNames = function() {
         });
 };
 
+function hashPassword(plainTextPassword) {
+    return new Promise(function(resolve, reject) {
+        bcrypt.genSalt(function(err, salt) {
+            if (err) {
+                return reject(err);
+            }
+            bcrypt.hash(plainTextPassword, salt, function(err, hash) {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(hash);
+            });
+        });
+    });
+}
+
+function checkPassword(textEnteredInLoginForm, hashedPasswordFromDatabase) {
+    return new Promise(function(resolve, reject) {
+        bcrypt.compare(textEnteredInLoginForm, hashedPasswordFromDatabase, function(err, doesMatch) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(doesMatch);
+            }
+        });
+    });
+}
+
+function insertNewUser(first, last, email, hashedPw) {
+    const q = `INSERT INTO users (first, last, email, password) VALUES ($1, $2, $3, $4) RETURNING id`;
+    const params = [first || null, last || null, email || null, hashedPw || null];
+
+    return db.query(q, params);
+}
+
+const showHashPw = function (email) {
+    return db.query(`SELECT password FROM users WHERE email = $1`, [email])
+        .then(function(result) {
+            return result.rows[0].password;
+        })
+}
+
 exports.setSig = setSig;
 exports.showSig = showSig;
 exports.showTotalSigners = showTotalSigners;
 exports.showNames = showNames;
+exports.hashPassword = hashPassword;
+exports.checkPassword = checkPassword;
+exports.insertNewUser = insertNewUser;
+exports.showHashPw = showHashPw;
+exports.getLoginId = getLoginId;
+exports.getUserName = getUserName;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const hb = require('express-handlebars');
-const database = require('./database.js');
+const db = require('./database.js');
 const csurf = require('csurf');
 app.engine('handlebars', hb());
 app.set('view engine', 'handlebars');
@@ -19,24 +19,106 @@ app.use(csurf());
 app.use(function(req, res, next) {
     res.locals.csrfToken = req.csrfToken();
     next();
-});	
+});
+
+function checkForUser(req, res, next) {
+	if (!req.session.userId) {
+		res.redirect('/register');
+	} else {
+		next();
+	}
+}
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    if (req.session.sigId) {
-        res.redirect('/success')
-    } else {
-        res.render('home', {
+    res.redirect('/register');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register', {
+        layout: 'main',
+        title: 'Registration'
+    })
+});
+
+app.post('/register', (req, res) => {
+    db.hashPassword(req.body.password)
+    .then(hash => {
+        return db.insertNewUser(req.body.first, req.body.last, req.body.email, hash).then(results => {
+            req.session.userId = results.rows[0].id;
+            res.redirect('/home');
+        })
+    })
+    .catch(err => {
+        res.render('register', {
+            myErr: err.message,
             layout: 'main',
-            title: 'Home Page'
+            title: 'Registration'
+        })
+    })
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {
+        layout: 'main',
+        title: 'Login'
+    })
+});
+
+app.post('/login', (req, res) => {
+    db.showHashPw(req.body.email).then(userPw => {
+        db.checkPassword(req.body.password, userPw);
+        db.getLoginId(req.body.email).then(id => {
+            req.session.userId = id;
+            res.redirect('/home');
+        })
+        .catch(err => {
+            res.render('login', {
+                myErr: err.message,
+                layout: 'main',
+                title: 'Login'
+            })
+        })
+    })
+    .catch(err => {
+        console.log("yo made it!");
+        res.render('login', {
+            myErr: err.message,
+            layout: 'main',
+            title: 'Login'
+        })
+    })
+});
+
+app.get('/home', checkForUser, (req, res) => {
+    if (req.session.sigId) {
+        res.redirect('/success');
+    } else {
+        db.getUserName(req.session.userId)
+        .then(info => {
+            console.log(info);
+            res.render('home', {
+                layout: 'main',
+                title: 'Home Page',
+                userFirst: info.first,
+                userLast: info.last
+            })
         })
     }
 });
 
-app.get('/success', (req, res) => {
-    database.showTotalSigners().then(num => {
-        database.showSig(req.session.sigId).then(sig => {
+app.post('/home', (req, res) => {
+    db.setSig(req.body.first,req.body.last,req.body.sig)
+    .then(id => {
+        req.session.sigId = id;
+        res.redirect('/success');
+    })
+});
+
+app.get('/success', checkForUser, (req, res) => {
+    db.showTotalSigners().then(num => {
+        db.showSig(req.session.sigId).then(sig => {
             res.render('success', {
                 layout: 'main',
                 title: 'Success Page',
@@ -46,16 +128,8 @@ app.get('/success', (req, res) => {
     });
 });
 
-app.post('/', (req, res) => {
-    database.setSig(req.body.first,req.body.last,req.body.sig)
-    .then(id => {
-        req.session.sigId = id;
-        res.redirect('/success');
-    })
-});
-
-app.get('/signers', (req, res) => {
-    database.showNames().then(signers => {
+app.get('/signers', checkForUser, (req, res) => {
+    db.showNames().then(signers => {
         res.render('signers', {
             layout: 'main',
             title: 'Others Who Signed',
@@ -65,12 +139,3 @@ app.get('/signers', (req, res) => {
 });
 
 app.listen(8080, () => console.log('Listening on port 8080'));
-
-/*
-CREATE TABLE signatures(
-   id SERIAL PRIMARY KEY,
-   first VARCHAR(100) NOT NULL,
-   last VARCHAR(200) NOT NULL,
-   signature TEXT NOT NULL
-);
-*/
